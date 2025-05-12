5d9bbaede1d1e087b10d9d632fb07eaa48688929
Tou102 <kiet.rime@gmail.com>
2025-05-12 21:29:30 +0700

    thay doi ten Calo va Nutrition

--- app/src/main/java/com/example/fitness/CaloCaculate.kt
+++ app/src/main/java/com/example/fitness/CaloCaculate.kt
@@ -3,7 +3,6 @@ package com.example.fitness
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
